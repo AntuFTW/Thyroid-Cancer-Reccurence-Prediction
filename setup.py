64a6d21c963be8642b52f68f2ad86a624463bdf9
Antariksh Patel <antarikshpatel2002@gmail.com>
2024-06-10 16:18:34 +0100

    yeah

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirments(file_path:str)->List[str]:
     reuqirments = []
     with open(file_path) as file_obj:
         requirments = file_obj.readlines()
-        requirments = [req.replace("\n") for req in requirments]
+        requirments = [req.replace("\n", "") for req in requirments]
         
         if HYPEN_E_DOT in requirments:
             requirments.remove(HYPEN_E_DOT)
@@ -18,10 +18,9 @@ def get_requirments(file_path:str)->List[str]:
 
 setup(
 name='first_ml_project',
-version='0.0.1'.
+version='0.0.1',
 author='Ant',
 author_email='antarikshpatel2002@gmail.com',
 packages = find_packages(),
 install_requires = get_requirments('requirments.txt')
-
 )
